fed1d4db21061537e4d25b160304f630a3b4e959
Muhammad Ahsan <ahsan.ahsan.2977@gmail.com>
2026-09-19 20:58:25 +0500

    Remove experimental Asyncify loading yield

--- src/core/main.cpp
+++ src/core/main.cpp
@@ -805,12 +805,6 @@ LoadingScreen(const char *str1, const char *str2, const char *splashscreen)
  		DoRWStuffEndOfFrame();
 	}
 
-#ifdef __EMSCRIPTEN__
-	// LoadingScreen() is called between the major synchronous startup phases.
-	// Yield one browser turn here so the page can paint the loading UI, process
-	// input and avoid being reported as unresponsive during world/model setup.
-	emscripten_sleep(0);
-#endif
 }
 
 void
